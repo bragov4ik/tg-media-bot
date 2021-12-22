9fe39d588846668e1df9e9438df428d25588e4ef
Kirill Ivanov <bragov4ik@hotmail.com>
2021-12-23 01:21:57 +0300

    Layout of main logic, new connection to DB

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,7 @@
 use redis::AsyncCommands;
 use redis::RedisResult;
+use serde::de::DeserializeOwned;
+use serde::{ Serialize, Deserialize };
 use crate::logs::format_log_chat;
 use log::info;
 /*
@@ -12,7 +14,7 @@ pub struct RedisConnection {
 }
 
 impl RedisConnection {
-    async fn new(redis_ip: &str) -> redis::RedisResult<RedisConnection> {
+    pub async fn new(redis_ip: &str) -> redis::RedisResult<RedisConnection> {
         let client = redis::Client::open(redis_ip)?;
         let con = client.get_async_connection().await?;
         Ok(RedisConnection{ connection: con })
@@ -25,9 +27,13 @@ impl RedisConnection {
         format!("chat:{}", chat_id)
     }
 
-    async fn set_alias(&mut self, chat_id: i64, sticker_id: &str, alias: &str) {
-        let chat_key: String = RedisConnection::get_chat_key(chat_id);
-        let set_result: RedisResult<()> = self.connection.hset(chat_key, alias, sticker_id).await;
+    fn get_aliases_key(chat_id: i64) -> String {
+        RedisConnection::get_chat_key(chat_id) + "aliases"
+    }
+
+    pub async fn set_alias(&mut self, chat_id: i64, alias: &str, sticker_id: &str) {
+        let key: String = RedisConnection::get_aliases_key(chat_id);
+        let set_result: RedisResult<()> = self.connection.hset(key, alias, sticker_id).await;
         match set_result {
             Ok(_) => {
                 info!("{}", format_log_chat(&format!("Saved alias '{a}' for '{s}'", a=alias, s=sticker_id), chat_id));
@@ -38,7 +44,7 @@ impl RedisConnection {
         }
     }
 
-    async fn set_aliases<'a, T>(&mut self, chat_id: i64, sticker_id: &str, aliases: T) 
+    pub async fn set_aliases<'a, T>(&mut self, chat_id: i64, aliases: T, sticker_id: &str) 
         where
             T: IntoIterator<Item = &'a str> {
         for alias in aliases {
@@ -46,9 +52,9 @@ impl RedisConnection {
         }
     }
 
-    async fn get_sticker_id(&mut self, chat_id: i64, alias: &str) -> Option<String> {
-        let chat_key: String = RedisConnection::get_chat_key(chat_id);
-        let set_result: RedisResult<String> = self.connection.hget(chat_key, alias).await;
+    pub async fn get_sticker_id(&mut self, chat_id: i64, alias: &str) -> Option<String> {
+        let key: String = RedisConnection::get_aliases_key(chat_id);
+        let set_result: RedisResult<String> = self.connection.hget(key, alias).await;
         match set_result {
             Ok(sticker_id) => {
                 info!("{}", format_log_chat(&format!("Retrieved '{s}' by alias '{a}'", a=alias, s=sticker_id), chat_id));
@@ -58,6 +64,106 @@ impl RedisConnection {
                 info!("{}", format_log_chat(&format!("Failed find alias '{a}' in DB: {}", e, a=alias), chat_id));
                 None
             }
+        }
+    }
+
+    pub async fn remove_alias(&mut self, chat_id: i64, alias: &str) {
+        let key: String = RedisConnection::get_aliases_key(chat_id);
+        let del_result: RedisResult<()> = self.connection.hdel(key, alias).await;
+        match del_result {
+            Ok(_) => {
+                info!("{}", format_log_chat(&format!("Removed alias '{a}'", a=alias), chat_id));
+            }
+            Err(e) => {
+                info!("{}", format_log_chat(&format!("Failed to remove alias from DB: {}", e), chat_id));
+            }
+        }
+    }
+}
+
+/// An error returned from `Storage` implementation.
+#[derive(Debug)]
+pub enum RedisStorageError
+{
+    SerdeError(serde_json::Error),
+
+    RedisError(redis::RedisError),
+
+    /// Returned from [`RedisStorage::remove_dialogue`].
+    DialogueNotFound,
+}
+
+// Dialogue storage
+impl RedisConnection {
+    fn get_dialogues_key(chat_id: i64) -> String {
+        RedisConnection::get_chat_key(chat_id) + "dialogues"
+    }
+
+    fn get_from_field(from_id: Option<i64>) -> String {
+        from_id.map(|x| x.to_string()).unwrap_or("NO_ID".to_string())
+    }
+
+    pub async fn update_dialogue<'a, D>(
+        &mut self,
+        chat_id: i64,
+        from_id: Option<i64>,
+        dialogue: D,
+    ) -> Result<(), RedisStorageError>
+    where
+        D: Serialize {
+        let key: String = RedisConnection::get_dialogues_key(chat_id);
+        let field: String = RedisConnection::get_from_field(from_id);
+
+        // Serialize
+        let value: String = serde_json::to_string(&dialogue).map_err(|err|  {
+            info!("{}", format_log_chat(&format!("Failed to serialize dialogue: {}", err), chat_id));
+            RedisStorageError::SerdeError(err)
+        })?;
+
+        // Save
+        let set_result: RedisResult<()> = self.connection.hset(&key, &field, &value).await;
+        match &set_result {
+            Ok(_) => {
+                info!("{}", format_log_chat(&format!("Saved dialogue for '{f}'", f=field), chat_id));
+            }
+            Err(err) => {
+                info!("{}", format_log_chat(&format!("Failed to save dialogue to DB: {}", err), chat_id));
+            }
+        }
+        set_result.map_err( RedisStorageError::RedisError)
+    }
+
+    pub async fn get_dialogue<'a, D>(
+        &mut self,
+        chat_id: i64,
+        from_id: Option<i64>,
+    ) -> Result<Option<D>, RedisStorageError>
+    where
+        D: DeserializeOwned {
+        let key: String = RedisConnection::get_dialogues_key(chat_id);
+        let field: String = RedisConnection::get_from_field(from_id);
+
+        // Retrieve from DB
+        let value: Option<String> = self.connection.hget(&key, &field).await.map_err(RedisStorageError::RedisError)?;
+
+        // Deserialize
+        let value: Result<Option<D>, RedisStorageError> = value.map(|v| serde_json::from_str::<D>(&v[..])).transpose().map_err(RedisStorageError::SerdeError);
+        value
+    }
+
+    pub async fn remove_dialogue(
+        &mut self,
+        chat_id: i64,
+        from_id: Option<i64>,
+    ) -> Result<(), RedisStorageError> {
+        let key: String = RedisConnection::get_dialogues_key(chat_id);
+        let field: String = RedisConnection::get_from_field(from_id);
+
+        let del_res: RedisResult<i64> = self.connection.hdel(key, field).await;
+        match del_res {
+            Ok(0) => Err(RedisStorageError::DialogueNotFound),
+            Ok(_) => Ok(()),
+            Err(e) => Err(RedisStorageError::RedisError(e)),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,14 @@ mod dialogue;
 mod logs;
 mod db;
 
-use crate::dialogue::{Answer, Dialogue};
 use teloxide::prelude::*;
-use teloxide::types::{MediaKind, MessageKind};
 use tokio_stream::wrappers::UnboundedReceiverStream;
+
+use std::sync::Arc;
+// TODO: get rid of using tokio's Mutex https://tokio.rs/tokio/tutorial/channels
+use tokio::sync::Mutex;
+
+use crate::db::RedisConnection;
 
 #[tokio::main]
 async fn main() {
@@ -24,31 +28,100 @@ async fn run() {
     //         .expect("Some problem happened")
     // })
     // .await;
+    
+    let args: Vec<String> = std::env::args().collect();
+    let config = parse_args(args);
 
-    Dispatcher::new(bot).messages_handler(handle_message).dispatch().await;
-    log::info!("Closing the bot...");
+    let db_shared: Arc<Mutex<RedisConnection>> = Arc::new(Mutex::new(
+        match db::RedisConnection::new(&config.redis_ip[..]).await {
+            Ok(v) => v,
+            Err(err) => panic!("Could not start redis connection: {}", err),
+        }
+    ));
+
+    Dispatcher::new(bot)
+        .messages_handler(|rx: UnboundedReceiver<UpdateWithCx<AutoSend<Bot>, Message>>| async move {
+            UnboundedReceiverStream::new(rx).for_each_concurrent(None, |cx | async {
+                let new_db_handle = Arc::clone(&db_shared);
+                handle_message(cx.update, new_db_handle).await
+            }).await;
+        }
+        ).dispatch().await;
+        log::info!("Closing the bot...");
 }
 
-async fn handle_message(rx: DispatcherHandlerRx<AutoSend<Bot>, Message>) {
-    UnboundedReceiverStream::new(rx).for_each_concurrent(None, )
+struct Config {
+    redis_ip: String
+}
+
+fn parse_args(args: Vec<String>) -> Config {
+    match args.len() {
+        1 => {
+            Config{ redis_ip: String::from("redis://127.0.0.1/") }
+        }
+        2 => {
+            Config{ redis_ip: String::from("redis://") + &args[1][..] + "/" }
+        }
+        _ => {
+            print_usage();
+            panic!();
+        }
+    }
+}
+
+fn print_usage() {
+    println!("Telegram bot. Run with no arguments or specify redis ip as first argument (without 'redis://' prefix).")
+}
+
+async fn handle_message(msg: Message, db: Arc<Mutex<RedisConnection>>) {
+    use teloxide::types::{MediaKind, MessageKind};
+    use crate::dialogue::{Answer, Dialogue};
+
+    // Don't know hot to avoid repeating of this code properly
+    fn default_response(
+        cx: UpdateWithCx<AutoSend<Bot>, Message>,
+        dialogue: Dialogue,
+    ) -> TransitionOut<Dialogue> {
+        log::info!(
+            "{}",
+            logs::format_log_chat("Received something else", cx.chat_id())
+        );
+        cx.answer("Send a sticker to start.");
+        next(dialogue)
+    }
+    
+
+
+    match msg.kind {
+        MessageKind::Common(cmn) => {
+        let ans: Answer;
+        match &cmn.media_kind {
+            MediaKind::Text(media) => {
+                log::info!("{}", logs::format_log_chat("Received a text", cx.chat_id()));
+                ans = Answer::String(media.text.clone());
+            }
+            MediaKind::Sticker(media) => {
+                log::info!(
+                    "{}",
+                    logs::format_log_chat("Received a sticker", cx.chat_id())
+                );
+                ans = Answer::Sticker(media.sticker.clone());
+            }
+            _ => {
+                default_response(cx, dialogue);
+            }
+        }
+        let res = dialogue.react(cx, ans).await;
+        res
+        }
+        _ => default_response(cx, dialogue),
+    }
 }
 
 // async fn handle_message(
 //     cx: UpdateWithCx<AutoSend<Bot>, Message>,
 //     dialogue: Dialogue,
 // ) -> TransitionOut<Dialogue> {
-//     // Don't know hot to avoid repeating of this code properly
-//     fn default_response(
-//         cx: UpdateWithCx<AutoSend<Bot>, Message>,
-//         dialogue: Dialogue,
-//     ) -> TransitionOut<Dialogue> {
-//         log::info!(
-//             "{}",
-//             logs::format_log_chat("Received something else", cx.chat_id())
-//         );
-//         cx.answer("Send a sticker to start.");
-//         next(dialogue)
-//     }
 
 //     match &cx.update.kind {
 //         MessageKind::Common(cmn) => {
