0391419ec3595c713b5a95c23782658def0e5cc6
Kirill Ivanov <bragov4ik@hotmail.com>
2021-12-23 13:54:27 +0300

    Add comments in main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,14 @@ async fn main() {
     run().await;
 }
 
+/// Main run function.
+/// 
+/// Sets everything up and starts the bot.
 async fn run() {
     teloxide::enable_logging!();
     log::info!("Starting dialogue bot...");
 
     let bot = Bot::from_env().auto_send();
-
-    // teloxide::dialogues_repl(bot, |message, dialogue| async move {
-    //     handle_message(message, dialogue)
-    //         .await
-    //         .expect("Some problem happened")
-    // })
-    // .await;
 
     let args: Vec<String> = std::env::args().collect();
     let config = parse_args(args);
@@ -56,10 +52,16 @@ async fn run() {
     log::info!("Closing the bot...");
 }
 
+/// Application configuration.
+/// 
+/// Contains info necessary for running the bot, such as IP address of redis
 struct Config {
     redis_ip: String,
 }
 
+/// Parse config from splitted arguments.
+/// 
+/// Assumes `std::env::args().collect()` ordering and length of vector.
 fn parse_args(args: Vec<String>) -> Config {
     match args.len() {
         1 => Config {
@@ -75,10 +77,16 @@ fn parse_args(args: Vec<String>) -> Config {
     }
 }
 
+/// Print out usage of the application in standard output
 fn print_usage() {
     println!("Telegram bot. Run with no arguments or specify redis ip as first argument (without 'redis://' prefix).")
 }
 
+/// Handle message update in context of dialogue.
+/// 
+/// Log special cases such as receiving text or sticker, prepare and provide `Answer` argument for dialogue.
+/// 
+/// Returns result of the handling that contains `DialogueStage` (uses `teloxide` dialogues, find details there).
 async fn handle_dialogue(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
     dialogue: Dialogue,
@@ -95,12 +103,14 @@ async fn handle_dialogue(
             "{}",
             logs::format_log_chat("Received something else", cx.chat_id())
         );
+        // TODO: maybe ignore the case (not answer anything?)
         cx.answer("Send a sticker to start.");
         next(dialogue)
     }
 
     match &cx.update.kind {
         MessageKind::Common(cmn) => {
+            // Parse `Answer` with logging the process.
             let ans: Answer;
             match &cmn.media_kind {
                 MediaKind::Text(media) => {
@@ -118,19 +128,24 @@ async fn handle_dialogue(
                     return default_response(cx, dialogue);
                 }
             }
-            let res = dialogue.react(cx, ans).await;
-            res
+
+            // Forward the user answer to dialogue to handle.
+            dialogue.react(cx, ans).await
         }
         _ => default_response(cx, dialogue),
     }
 }
 
+/// Handle message update.
+/// 
+/// Find `Dialogue` for `handle_dialogue` from db. Use the function result to update dialogue state in database.
 async fn handle_message(
     cx: UpdateWithCx<AutoSend<Bot>, Message>,
     db_shared: Arc<Mutex<RedisConnection>>,
 ) {
     let mut db_con = db_shared.lock().await;
 
+    // Obtain dialogue from database
     let chat_id = cx.update.chat_id();
     let from_id = cx.update.from().map(|u| u.id);
     let dialogue: Dialogue = match db_con
@@ -155,6 +170,7 @@ async fn handle_message(
         }
     };
 
+    // Handle the dialogue and receive results.
     let stage = match handle_dialogue(cx, dialogue).await {
         Ok(a) => a,
         Err(e) => {
@@ -173,6 +189,7 @@ async fn handle_message(
         }
     };
 
+    // Update the dialogue state in database.
     match stage {
         DialogueStage::Next(new_dialogue) => {
             if let Err(e) = db_con.update_dialogue(chat_id, from_id, new_dialogue).await {
